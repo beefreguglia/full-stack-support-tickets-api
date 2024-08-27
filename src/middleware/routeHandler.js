a1ecfe4955ba6b624af7483e9bb7456d86e61d4a
Bernardo <beefreguglia@gmail.com>
2024-08-27 16:50:03 -0300

    feature: prepare routes to user dinamic parameters

--- src/middleware/routeHandler.js
+++ src/middleware/routeHandler.js
@@ -5,7 +5,10 @@ const database = new Database()
 
 export function routeHandler({ request, response }) {
   const route = routes
-    .find((route) => route.method === request.method && route.path === request.url)
+    .find(
+      (route) => route.method === request.method 
+        && route.path.test(request.url)
+    )
 
   if (route) {
     return route.controller({ request, response, database })
