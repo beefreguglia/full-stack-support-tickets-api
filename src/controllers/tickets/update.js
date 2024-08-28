8b47b99e316f60bd236ba7c7a16526793868391f
Bernardo <beefreguglia@gmail.com>
2024-08-27 21:02:51 -0300

    feature: finish update route

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -2,5 +2,12 @@ export function update({ request, response, database }) {
   const { id } = request.params
   const { equipment, description, user_name } = request.body
 
+  database.update("tickets", id, {
+    equipment,
+    description,
+    user_name,
+    updated_at: new Date(),
+  })
+
   return response.end()
 }
